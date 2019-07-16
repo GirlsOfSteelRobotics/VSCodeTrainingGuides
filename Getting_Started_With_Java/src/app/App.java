b4a8b136a5c372550216575b6edf1e4c0bb5c4ba
Claire <claire.e.cummings@gmail.com>
2019-07-16 19:43:45 -0400

    java fixed?

---
+++ Getting_Started_With_Java/src/app/App.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // write your own code here
+        // these are comments
+
+        /*
+         * this is a multi-line comment, enclosed by "/" and "*" it's still a comment
+         * this is the last line of it
+         */
+
+        // let's declare a variable x that has an integer type
+        int x; /* remember to add semi-colon after each line of code */
+
+        // now i'm assigning a value to x
+        x = 3;
+
+        // add your practice code down below
+
+    } // don't add anything below or on this line }
+}
