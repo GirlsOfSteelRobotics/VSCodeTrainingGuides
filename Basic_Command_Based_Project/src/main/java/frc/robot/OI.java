7624d824b60498431bab46a849e2eb4704519d84
Claire <claire.e.cummings@gmail.com>
2019-07-17 15:15:20 -0400

    removing the filled out portions of code part two

--- Basic_Command_Based_Project/src/main/java/frc/robot/OI.java
+++ Basic_Command_Based_Project/src/main/java/frc/robot/OI.java
@@ -5,16 +5,22 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team3504.robot;
+package frc.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 
-import org.usfirst.frc.team3504.robot.commands.*;
+import frc.robot.commands.*;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	
+
 }
